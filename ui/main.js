136604dcb307934a2f76fb0bc204b903e0476fc1
Mounika243 <30685500+Mounika243@users.noreply.github.com>
2017-08-21 15:35:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
 
@@ -12,4 +12,28 @@ function moveRight() {
 
 img.onclick= function(){
      var interval=setInterval(moveRight,50);
+};*/
+
+
+//counter code
+var button=document.getElementByID('counter');
+var counte=0;
+button.onclick= function(){
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+      if(statechange==XMLHttpRequest.DONE)
+      {
+          if(request.status==200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+        
+    };
+    
+    request.open('GET','http://http://mounikanandikonda243.imad.hasura-app.io/counter',true)
+    request.send(null);
 };
