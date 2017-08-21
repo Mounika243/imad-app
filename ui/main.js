1491c404605eac50e1fc7de7f2da2acbb55fcfe1
Mounika243 <30685500+Mounika243@users.noreply.github.com>
2017-08-21 17:00:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,6 +64,6 @@ var submit=document.getElementById('submit_btn');
     };
         var nameInput=document.getElementById('name');
          var name=nameInput.value;
-      request.open('GET','http://mounikanandikonda243.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+         request.open('GET','http://mounikanandikonda243.imad.hasura-app.io/submit-name?name='+name,true);
+          request.send(null);
  };
